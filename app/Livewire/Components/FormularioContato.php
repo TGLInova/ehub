824ba44fdd2942d04794481f9afab8d9149377e0
Wallace <wallacemaxters@gmail.com>
2025-01-28 15:30:25 -0300

    menu

--- app/Livewire/Components/FormularioContato.php
+++ app/Livewire/Components/FormularioContato.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class FormularioContato extends Component
 {
+    public array $form = [];
+
     public function render()
     {
         return view('livewire.components.formulario-contato');
--- resources/views/vendor/bless-ui/components/navbar.blade.php
+++ resources/views/vendor/bless-ui/components/navbar.blade.php
@@ -1,10 +1,14 @@
 @props(['links' => []])
 <x-bless-ui::wrapper {{ $attributes }} tag="header" :tag-self-close="false" component="navbar">
-    <x-ui::container class="flex">
+    <x-ui::container class="flex items-center justify-between gap-4"  x-data="{mobileOpen: false}">
         <a href="{{ route('home') }}" wire:navigate>
             <x-icon name="logo" />
         </a>
-        <nav class="flex items-center justify-end grow gap-12 max-lg:hidden">
+        <nav @class([
+            'flex lg:items-center lg:justify-end justify-center grow gap-12',
+            'max-lg:fixed max-lg:flex-col max-lg:w-full max-lg:h-full max-lg:top-0 max-lg:left-0 max-lg:bg-primary-500/90 max-lg:z-10',
+            'max-lg:p-6'
+        ]) x-bind:class="{'max-lg:hidden' : !mobileOpen}">
             @foreach($links as $url => $text)
                 <a href="{{ $url }}" wire:navigate>{{ $text }}</a>
             @endforeach
@@ -12,5 +16,8 @@
                 Quero contratar
             </x-ui::button>
         </nav>
+        <a class="w-12 h-12 lg:hidden" x-on:click="mobileOpen = true">
+            <x-icon name="heroicon-o-bars-3" />
+        </a>
     </x-ui::container>
 </x-bless-ui::wrapper>
